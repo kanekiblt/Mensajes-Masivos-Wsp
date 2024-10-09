5c590ef50752f1dcce5c0f044046a5a318b24c6f
KANEKI <antonyboyer980@gmail.com>
2024-10-08 21:39:13 -0500

    Create WhastApp.py

---
+++ WhastApp.py
@@ -0,0 +1,14 @@
+import pyautogui as pg
+import time
+
+msjs = int(input("Número de mensajes: "))
+ms = "" <---//Escribe el mensaje que quieres enviar
+
+time.sleep(3)
+
+for i in range(msjs):
+   
+    pg.write(ms)
+    #pg.write(msj)
+    
+    pg.press('enter')
